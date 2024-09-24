073d9386b92a8e9af78933941307a46ceb2c3c38
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-09-24 18:06:37 -0300

    Adicionado migrações
    - Adicionadas migrações para refletir as alterações nos modelos Aluno e Professores.

--- obj/Debug/net8.0/Gestao.AssemblyInfo.cs
+++ obj/Debug/net8.0/Gestao.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78f9a0e3de06b6c136765d089e25ade8bfda2296")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a9dd5bb695a839fad89e65b891e26cacf24131a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
