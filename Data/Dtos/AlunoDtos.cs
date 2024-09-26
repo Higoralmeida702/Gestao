5c37d2c012dc3c82db0ee554c4cd76f9e93131f9
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-09-25 23:38:26 -0300

    Adiciona DTOs para Aluno e Professor
    - Criado o DTO AlunoDtos com validações para Nome, Usuário, Email, Senha, Confirmação de Senha, Número, Endereço, Data de Nascimento e Cargo.
    
    - Adicionado um DTO básico para Professores, pronto para implementação futura.

---
+++ Data/Dtos/AlunoDtos.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Gestao.Enum;
+
+namespace Gestao.Data.Dtos
+{
+    public class AlunoDtos
+    {
+       [Required(ErrorMessage = "Digite um nome!")]
+       public string Nome {get; set;}
+
+       [Required(ErrorMessage = "Insira um nome de usuario valido!")] 
+       public string Usuario {get; set;}
+       [Required(ErrorMessage = "Digite um email valido!")]
+       public string Email {get; set;}
+       [Required(ErrorMessage = "Digite uma senha valida")]
+       public string Senha {get; set;}
+       [Required]
+       [Compare("Senha", ErrorMessage = "Senha nao coincidem")]
+       public string ConfirmacaoSenha {get; set;} 
+       [Required(ErrorMessage = "Digite um numero valido!")]
+       public string Numero {get; set;}
+       [Required(ErrorMessage = "Digite um endereço valido!")]
+       public string Endereco {get; set;}
+       [Required(ErrorMessage = "Coloque uma data valida")]
+       public DateTime DataNascimento {get; set;}
+       public CargoEnum Cargo {get; set;}
+    }
+}
