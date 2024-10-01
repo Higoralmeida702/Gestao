f067ca868582a178a0487022459798c804c08fd7
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-09-30 21:27:05 -0300

    Implementação do Serviço de Autenticação para Professores
    -Adição da classe ProfessorAuthService, responsável pela autenticação e registro de professores.
    -O serviço inclui métodos para registrar um novo professor e fazer login.

---
+++ Service/AuthService/IProfessorAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Gestao.Data.Dtos;
+using Gestao.Model;
+
+namespace Gestao.Service.AuthService
+{
+    public interface IProfessorAuthService
+    {
+        Task<Response<ProfessorDto>> Registrar (ProfessorDto professorDto);
+        Task<Response<string>> Login (LoginDto loginDto);
+    }
+}
---
+++ Service/AuthService/ProfessorAuthService.cs
@@ -0,0 +1,105 @@
+﻿using Gestao.Data;
+using Gestao.Data.Dtos;
+using Gestao.Enum;
+using Gestao.Model;
+using Gestao.Service.AuthService.SenhaService;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gestao.Service.AuthService
+{
+    public class ProfessorAuthService : IProfessorAuthService
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        private readonly ISenhaService _iSenhaService;
+
+        public ProfessorAuthService (ApplicationDbContext applicationDbContext, ISenhaService senhaService)
+        {
+            _applicationDbContext = applicationDbContext;
+            _iSenhaService = senhaService;
+        }
+
+        public async Task<Response<ProfessorDto>> Registrar(ProfessorDto professorDto)
+        {
+            Response<ProfessorDto> respostaServico = new Response<ProfessorDto>();
+            try
+            {
+                if(!VerificaSeEmaileUsuarioJaExistem(professorDto))
+                {
+                    respostaServico.Dados = null;
+                    respostaServico.Status = false;
+                    respostaServico.Mensagem = "Usuario ja cadastrado";
+                    return respostaServico;
+                }
+
+            _iSenhaService.CriarSenhaHash(professorDto.Senha,out byte[] senhaHash, out byte[] senhaSalt);
+
+            Professores usuario = new Professores()
+            {
+                Usuario = professorDto.Usuario,
+                Nome = professorDto.Nome,
+                Endereco = professorDto.Endereco,
+                Cargo = CargoEnum.Professor,
+                Numero = professorDto.Numero,
+                DataNascimento = professorDto.DataNascimento,
+                Email = professorDto.Email,
+                MateriaResponsavel = professorDto.MateriaResponsavel,
+                PasswordHash = senhaHash,
+                PasswordSalt = senhaSalt
+            };   
+            
+            _applicationDbContext.Add(usuario);
+            await _applicationDbContext.SaveChangesAsync();
+            respostaServico.Mensagem = "Professor criado com sucesso";
+
+            }
+            catch (Exception error)
+            {
+                respostaServico.Dados = null;
+                respostaServico.Mensagem = error.Message;
+                respostaServico.Status = false;
+            }
+            return respostaServico;
+        }
+
+        public async Task<Response<string>> Login(LoginDto loginDto)
+        {
+            Response<string> respostaServico = new Response<string>();
+            
+            try
+            {
+                var usuario = await _applicationDbContext.Professores.FirstOrDefaultAsync(userBanco => userBanco.Email == loginDto.Email);
+                if (usuario == null)
+                {
+                    respostaServico.Mensagem = "Crendencias inválidas!";
+                    respostaServico.Status = false;
+                    return respostaServico;
+                }
+
+                if(!_iSenhaService.VerificaSenhaHash(loginDto.Senha, usuario.PasswordHash, usuario.PasswordSalt))
+                {
+                    respostaServico.Mensagem = "Credenciais inválidas!";
+                    respostaServico.Status = false;
+                    return respostaServico;
+                }
+                var token = _iSenhaService.CriarToken(usuario);
+                respostaServico.Dados = token;
+                respostaServico.Mensagem = "Usuario logado com sucesso!";
+            }
+            catch (Exception error)
+            {
+                respostaServico.Dados = null;
+                respostaServico.Mensagem = error.Message;
+                respostaServico.Status = false;
+            }
+            return respostaServico;
+        }
+
+
+        public bool VerificaSeEmaileUsuarioJaExistem(ProfessorDto professorDto)
+        {
+            var usuario = _applicationDbContext.Professores.FirstOrDefault(userBanco => userBanco.Email == professorDto.Email || userBanco.Usuario == professorDto.Usuario);
+            if (usuario != null) return false;
+            return true;
+        }
+    }
+}
