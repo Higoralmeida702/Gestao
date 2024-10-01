3842b4a28445ae77789518c17c61a8be40808ffb
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-09-30 21:32:54 -0300

    Atualizado AuthController para suportar autenticação de alunos e professores
    - Substituído IAuthService por IAlunoAuthService e IProfessorAuthService.
    - Atualizados os métodos de registro e login para alunos e professores.
    - Adicionados endpoints para registrar e autenticar professores.

--- Controller/AuthController.cs
+++ Controller/AuthController.cs
@@ -12,26 +12,41 @@ namespace Gestao.Controller
     [Route("api/[controller]")]
     public class AuthController : ControllerBase
     {
-        private readonly IAuthService _iAuthService;
+        private readonly IAlunoAuthService _iAlunoAuthService;
+        private readonly IProfessorAuthService _iProfessorAuthService;
 
-        public AuthController(IAuthService authService)
+        public AuthController(IAlunoAuthService alunoAuthService, IProfessorAuthService iProfessorAuthService)
         {
-            _iAuthService = authService;
+            _iAlunoAuthService = alunoAuthService;
+            _iProfessorAuthService = iProfessorAuthService;
         }
 
         [HttpPost("Registrar/Aluno")]
-        public async Task<IActionResult> Register(AlunoDtos alunoDto)
+        public async Task<IActionResult> Register(AlunoDto alunoDto)
         {
-            var resposta = await _iAuthService.Registrar(alunoDto);
+            var resposta = await _iAlunoAuthService.Registrar(alunoDto);
             return Ok(resposta);
         }
 
         [HttpPost("Login/Aluno")]
         public async Task<IActionResult> Login(LoginDto loginDto)
         {
-            var resposta = await _iAuthService.Login(loginDto);
+            var resposta = await _iAlunoAuthService.Login(loginDto);
             return Ok(resposta);
         }
-       
+
+        [HttpPost("Registrar/Professor")]
+        public async Task<IActionResult> RegistrarProfessor(ProfessorDto professorDto)
+        {
+            var resposta = await _iProfessorAuthService.Registrar(professorDto);
+            return Ok(resposta);
+        }
+
+        [HttpPost("Login/Professor")]
+        public async Task<IActionResult> LoginProfessor(LoginDto ProfessorLoginDto)
+        {
+            var resposta = await _iProfessorAuthService.Login(ProfessorLoginDto);
+            return Ok(resposta);
+        }
     }
 }
