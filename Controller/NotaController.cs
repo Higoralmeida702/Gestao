26471437e40bfbb1f5e2b81a4e5e4e90a5a34065
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-10-08 20:40:00 -0300

    Implementação de Métodos no NotaController com Controle de Acesso para Professores
    Métodos Adicionados:
    -Obter RA por Usuário: Endpoint para buscar o RA de um aluno com base no nome de usuário.
    -Editar Nota: Endpoint para editar a nota de uma matéria específica, garantindo que a nova nota esteja dentro do intervalo válido.
    -Excluir Nota: Endpoint para excluir a nota de uma matéria específica.
    
    -Controle de Acesso: Implementado autorização nos endpoints do NotaController, permitindo acesso apenas a usuários autenticados com a claim de professor

--- Controller/NotaController.cs
+++ Controller/NotaController.cs
@@ -2,10 +2,12 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Gestao.Data.Dtos;
 using Gestao.Service;
+using Gestao.Enum;
 
 namespace Gestao.Controllers
 {
     [Route("api/[Controller]")]
+    [Authorize(Roles = nameof(CargoEnum.Professor))]
     [ApiController]
     public class NotaController : ControllerBase
     {
@@ -15,7 +17,6 @@ namespace Gestao.Controllers
         {
             _notaService = notaService;
         }
-
         [HttpPost("AtribuirNota")]
         public async Task<IActionResult> AtribuirNota([FromBody] NotaAlunoDto notaDto)
         {
@@ -37,5 +38,41 @@ namespace Gestao.Controllers
             return Ok(notas);
         }
 
+        [HttpGet("obterRA/{usuario}")]
+        public async Task<IActionResult> ObterRaPorUsuario(string usuario)
+        {
+            var ra = await _notaService.ObterRaPorUsuario(usuario);
+
+            if (ra.HasValue)
+            {
+                return Ok(new { RA = ra.Value });
+            }
+
+            return NotFound("Aluno não encontrado.");
+        }
+
+        [HttpPut("editarNota/{notaId}")]
+        public async Task<IActionResult> EditarNota(int notaId, [FromBody] double novaNota)
+        {
+            var resultado = await _notaService.EditarNota(notaId, novaNota);
+            if (resultado.StartsWith("Erro"))
+            {
+                return BadRequest(resultado);
+            }
+
+            return Ok(resultado);
+        }
+
+        [HttpDelete("excluirNota/{notaId}")]
+        public async Task<IActionResult> ExcluirNota(int notaId)
+        {
+            var resultado = await _notaService.ExcluirNota(notaId);
+            if (resultado.StartsWith("Erro"))
+            {
+                return BadRequest(resultado);
+            }
+
+            return Ok(resultado);
+        }
     }
 }
