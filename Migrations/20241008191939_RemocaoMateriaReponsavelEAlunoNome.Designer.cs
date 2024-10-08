3d9b4ad4fc939cf67f6ab145edd41b7df35ecf0b
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-10-08 18:32:34 -0300

    Criação da Migração: Remoção de Propriedades de MateriaResponsavel e Nome do Aluno
    -Criada a migração RemocaoMateriaReponsavelEAlunoNome para remover as propriedades MateriaResponsavel e Nome da tabela Alunos

--- Migrations/20241008191939_RemocaoMateriaReponsavelEAlunoNome.Designer.cs
+++ Migrations/20241008191939_RemocaoMateriaReponsavelEAlunoNome.Designer.cs
@@ -4,6 +4,7 @@ using Gestao.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Gestao.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241008191939_RemocaoMateriaReponsavelEAlunoNome")]
+    partial class RemocaoMateriaReponsavelEAlunoNome
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
